cd26b48fc08a4869452e70323e7f87b00eb15bfa
Daniel Hawton <daniel@hawton.com>
2019-05-30 16:45:15 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,22 @@
 local ragdoll = false
+ESX = nil
+local shownHelp = false
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+    end
+end)
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if IsControlJustPressed(2, 82) then
+        if IsControlJustPressed(2, 82) and not IsPedInAnyVehicle(GetPlayerPed(-1), false) then
             ragdoll = not ragdoll
+            if not ragdoll then
+                shownHelp = false
+            end
         end
         if Config.stunShouldRagdoll and IsPedBeingStunned(GetPlayerPed(-1)) then
             ragdoll = true
@@ -13,6 +25,11 @@ Citizen.CreateThread(function()
         -- Don't ragdoll if player is dead
         if IsPlayerDead(PlayerId()) and ragdoll == true then
             ragdoll = false
+            shownHelp = false
+        end
+        if ragdoll == true and not shownHelp then
+            ESX.ShowHelpNotification("Press ~INPUT_VEH_PREV_RADIO~ to stand up")
+            shownHelp = true
         end
     end
 end)
@@ -29,11 +46,17 @@ end)
 RegisterNetEvent('dh_ragdoll:toggle')
 AddEventHandler('dh_ragdoll:toggle', function()
     ragdoll = not ragdoll
+    if not ragdoll then
+        shownHelp = false
+    end
 end)
 
 RegisterNetEvent('dh_ragdoll:set')
 AddEventHandler('dh_ragdoll:set', function(value)
     ragdoll = value
+    if not ragdoll then
+        shownHelp = false
+    end
 end)
 
 RegisterCommand("rag", function (src, args, raw)
